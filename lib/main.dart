d7634372966556585eb6362865eae56960f3b9bb
wmfadel <mohamed.fadel24@hotmail.com>
2022-03-04 22:56:26 +0200

    Firebase Crashlytics Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
+import 'dart:async';
+import 'dart:isolate';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:shinchoku/shinchoku_app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const ShinchokuApp());
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+    runApp(const ShinchokuApp());
+    _errorListener();
+  }, (error, stack) => FirebaseCrashlytics.instance.recordError(error, stack));
+}
+
+_errorListener() {
+  Isolate.current.addErrorListener(RawReceivePort((pair) async {
+    final List<dynamic> errorAndStacktrace = pair;
+    await FirebaseCrashlytics.instance.recordError(
+      errorAndStacktrace.first,
+      errorAndStacktrace.last,
+      reason: 'From main._errorListener'
+    );
+  }).sendPort);
 }
