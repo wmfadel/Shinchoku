a02303201f403fd7083feb93da2407faa1542636
wmfadel <mohamed.fadel24@hotmail.com>
2022-03-04 17:23:37 +0200

    initial folder structure

---
+++ lib/shinchoku_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShinchokuApp extends StatelessWidget {
+  const ShinchokuApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
