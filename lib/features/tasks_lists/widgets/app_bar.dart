890bf5f3792fb8693fc004130870541e816b4c0d
wmfadel2 <m.fadel@awaed.co>
2022-05-05 23:18:18 +0200

    appbar content placeholder

--- lib/features/tasks_lists/widgets/app_bar.dart
+++ lib/features/tasks_lists/widgets/app_bar.dart
@@ -1,56 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shinchoku/core/widgets/shi_image.dart';
 import 'package:shinchoku/features/tasks_lists/utils/appbar_constants.dart';
+import 'package:shinchoku/features/tasks_lists/widgets/appbar_title.dart';
 
-import '../../../core/constants/images.dart';
-import '../../authentication/controllers/auth_bloc.dart';
 import '../utils/appbar_wave_clipper.dart';
 
 class SliverWaveAppbar extends SliverPersistentHeaderDelegate {
-  final double appbarMaxHeight = 320;
-  final double appbarMinHeight = 140;
-
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
-    AuthBloc authBloc = BlocProvider.of<AuthBloc>(context, listen: true);
-   // print(shrinkOffset);
-    return ClipPath(
-      clipper: AppbarWaveClipper(),
-      child: Container(
-        padding: EdgeInsets.all(12),
-        width: MediaQuery.of(context).size.width,
-        height: AppbarConstants.appbarMaxHeight,
-        color: Theme.of(context).colorScheme.secondary,
-        child: SafeArea(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('My Tasks',
-                      style: Theme.of(context).textTheme.headline2?.copyWith(color: Colors.white)),
-                  ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(90)),
-                    child: Container(
-                      width: 48,
-                      height: 48,
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.circle,
-                      ),
-                      child: ShiImage(
-                        authBloc.appUser?.image ?? Images.randomUserImage,
-                      ),
-                    ),
-                  ),
+    var adjustedShrinkOffset =
+        shrinkOffset > minExtent ? minExtent : shrinkOffset;
+    double offset = (minExtent - adjustedShrinkOffset) * 0.62;
+    double topPadding = MediaQuery.of(context).padding.top + 16;
+    print('adjust $adjustedShrinkOffset');
+    print('offset $offset');
+    print('shrink $shrinkOffset');
+    print('factor ${40 / shrinkOffset}');
+    return Stack(
+      children: [
+        ClipPath(
+          clipper: AppbarWaveClipper(),
+          child: Container(
+            padding: const EdgeInsets.all(12),
+            width: MediaQuery.of(context).size.width,
+            height: AppbarConstants.appbarMaxHeight,
+            color: Theme.of(context).colorScheme.secondary,
+            child: SafeArea(
+              child: Column(
+                children: const [
+                  AppbarTitle(),
                 ],
-              )
-            ],
+              ),
+            ),
           ),
         ),
-      ),
+        Positioned(
+          top: topPadding + offset,
+          left: 0,
+          right: 0,
+          child: AnimatedOpacity(
+            duration: const Duration(milliseconds: 500),
+            opacity: (40 / shrinkOffset) < 1
+                ? (40 / shrinkOffset) < 0.8
+                    ? 0
+                    : 40 / shrinkOffset
+                : 1,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 185,
+              color: Colors.green,
+            ),
+          ),
+        ),
+      ],
     );
   }
 
