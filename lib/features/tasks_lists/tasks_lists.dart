b4928516790690a3ffe093a307290123c50e88ad
wmfadel2 <m.fadel@awaed.co>
2022-05-05 22:48:09 +0200

    completed curves calculations

--- lib/features/tasks_lists/tasks_lists.dart
+++ lib/features/tasks_lists/tasks_lists.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shinchoku/features/authentication/controllers/auth_bloc.dart';
+import 'package:shinchoku/features/tasks_lists/widgets/app_bar.dart';
 
 class TasksListsPage extends StatelessWidget {
   const TasksListsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    AuthBloc bloc = BlocProvider.of<AuthBloc>(context, listen: true);
+    // AuthBloc authBloc = BlocProvider.of<AuthBloc>(context, listen: true);
 
     return Scaffold(
-      body: Center(
-        child: Text(bloc.appUser?.name ?? ''),
+      body: CustomScrollView(
+        slivers: [
+          SliverPersistentHeader(
+            delegate: SliverWaveAppbar(),
+            pinned: true,
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  margin: const EdgeInsets.all(8),
+                  padding: const EdgeInsets.all(16),
+                  child: const Text(
+                    'I\'m a placeholder text, doin\' my zing',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                );
+              },
+              childCount: 40,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/features/tasks_lists/widgets/app_bar.dart
+++ lib/features/tasks_lists/widgets/app_bar.dart
@@ -1,27 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shinchoku/core/widgets/shi_image.dart';
+import 'package:shinchoku/features/tasks_lists/utils/appbar_constants.dart';
 
-class TasksListAppbar extends StatelessWidget {
-  const TasksListAppbar({Key? key}) : super(key: key);
+import '../../../core/constants/images.dart';
+import '../../authentication/controllers/auth_bloc.dart';
+import '../utils/appbar_wave_clipper.dart';
 
-  static const double appbarHeight = 280;
+class SliverWaveAppbar extends SliverPersistentHeaderDelegate {
+  final double appbarMaxHeight = 320;
+  final double appbarMinHeight = 140;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    AuthBloc authBloc = BlocProvider.of<AuthBloc>(context, listen: true);
+   // print(shrinkOffset);
     return ClipPath(
       clipper: AppbarWaveClipper(),
       child: Container(
+        padding: EdgeInsets.all(12),
         width: MediaQuery.of(context).size.width,
-        height: appbarHeight,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          colors: [
-            Theme.of(context).primaryColor,
-            Theme.of(context).primaryColor.withOpacity(0.2),
-          ],
-        )),
+        height: AppbarConstants.appbarMaxHeight,
+        color: Theme.of(context).colorScheme.secondary,
+        child: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('My Tasks',
+                      style: Theme.of(context).textTheme.headline2?.copyWith(color: Colors.white)),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(90)),
+                    child: Container(
+                      width: 48,
+                      height: 48,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+                      child: ShiImage(
+                        authBloc.appUser?.image ?? Images.randomUserImage,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  @override
+  double get maxExtent => AppbarConstants.appbarMaxHeight;
+
+  @override
+  double get minExtent => AppbarConstants.appbarMinHeight;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return oldDelegate.maxExtent != maxExtent ||
+        oldDelegate.minExtent != minExtent;
+  }
 }
-
-
